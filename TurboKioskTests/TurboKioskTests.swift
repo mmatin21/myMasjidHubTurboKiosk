ab5172c83cc7f6be0457b974b00f23e2968f0e52
mmatin21 <makmatin17@gmail.com>
2025-04-01 22:29:00 -0400

    Initial Commit

---
+++ TurboKioskTests/TurboKioskTests.swift
@@ -0,0 +1,17 @@
+//
+//  TurboKioskTests.swift
+//  TurboKioskTests
+//
+//  Created by Osman Matin on 4/1/25.
+//
+
+import Testing
+@testable import TurboKiosk
+
+struct TurboKioskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
