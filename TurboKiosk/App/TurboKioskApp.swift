6ef252b9433727134bca6f0f3648495e51060c67
mmatin21 <makmatin17@gmail.com>
2025-04-02 21:10:52 -0400

    first commit

---
+++ TurboKiosk/App/TurboKioskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurboKioskApp.swift
+//  TurboKiosk
+//
+//  Created by Osman Matin on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TurboKioskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
